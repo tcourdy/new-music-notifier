c7c3f573c9b539b4745c2019c2cf41b48ce0e42d
tcourdy <tarik.courdy@gmail.com>
2017-07-20 21:39:02 -0600

    Added styling and sending twilio pin is working

--- src/App.js
+++ src/App.js
@@ -2,73 +2,197 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TextField from 'react-md/lib/TextFields';
+import Button from 'react-md/lib/Buttons/Button';
 
+
+class PhoneNumberInput extends Component {
+  render() {
+    return(
+        <TextField
+          label="Phone Number"
+          lineDirection="center"
+          placeholder="(123)456-7890"
+          value={this.props.value}
+          type="tel"
+          onChange={(val) => this.props.onChange(val)}
+        />
+    );
+  }
+}
+
+class PasswordInput extends Component {
+  render() {
+    return(
+      <TextField
+        label="Password"
+        lineDirection="center"
+        placeholder="Enter your password"
+        value={this.props.value}
+        type="password"
+        onChange={() => this.props.onChange()}
+      />
+    );
+  }
+}
+
+
+class RegisterView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      phoneNumber: '',
+      enableRegister: false
+    };
+    this.updatePhoneNumber = this.updatePhoneNumber.bind(this);
+    this.register = this.register.bind(this);
+  }
+
+  updatePhoneNumber(val) {
+    var phone = val.replace(/\D+/g, '');
+    if(phone.length === 10 && !Number.isNaN(Number.parseInt(phone, 10))) {
+      this.setState({
+        phoneNumber: val,
+        enableRegister: true
+      });
+    } else {
+      this.setState({
+        phoneNumber: val,
+        enableRegister: false
+      });
+    }
+  }
+
+  //TODO
+  register() {
+    var init = {
+      method: 'POST',
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "phoneNumber": this.state.phoneNumber.replace(/\D+/g, '')
+      })
+    };
+    ///sendTwilioPin
+    fetch('/sendTwilioPin', init)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      });
+  }
+
+  render() {
+    return(
+      <div>
+        <PhoneNumberInput onChange={this.updatePhoneNumber} />
+        <Button raised
+                label="Register"
+                onClick={this.register}
+                disabled={!this.state.enableRegister}
+        />
+      </div>
+    );
+  }
+}
 
 class LoginView extends Component {
   constructor() {
     super();
     this.state = {
       phoneNumber: '',
-      password: ''
+      password: '',
+      enableLogin: false
     };
 
-    this.handleLogin = this.handleLogin.bind(this);
+    this.login = this.login.bind(this);
     this.updatePassword = this.updatePassword.bind(this);
     this.updatePhoneNumber = this.updatePhoneNumber.bind(this);
   }
 
-  handleLogin() {
+  //TODO:
+  login() {
   }
 
   updatePassword(val) {
-    this.state.set({
-      password: val
-    });
+    if(val
+       && this.state.phoneNumber
+       && this.state.phoneNumber.replace(/\D+/g, '')){
+      this.setState({
+        password: val,
+        enableLogin: true
+      });
+    } else {
+      this.setState({
+        password: val
+      });
+    }
   }
 
   updatePhoneNumber(val) {
-    this.state.set({
-      phoneNumber: val
-    });
+    var phone = val.replace(/\D+/g, '');
+    if(phone.length === 10 && !Number.isNaN(Number.parseInt(phone, 10))
+       && this.state.password) {
+      this.setState({
+        phoneNumber: val,
+        enableLogin: true
+      });
+    } else {
+      this.setState({
+        phoneNumber: val,
+        enableLogin: true
+      });
+    }
   }
-
-  
 
   render() {
     return(
       <div>
-        <TextField
-          id="phoneNumber"
-          label="Phone Number"
-          lineDirection="center"
-          placeholder="(123)456-7890"
-        onChange={updatePhoneNumber}
+        <PhoneNumberInput onChange={this.updatePhoneNumber} />
+        <PasswordInput onChange={this.updatePassword} />
+        <Button raised
+                label="Login"
+                onClick={this.login}
+                disabled={!this.state.enableLogin}
         />
-        <TextField
-          id="password"
-          label="Enter your password"
-          type="password"
-        onChange={updatePassword}
-        />
-
-        <Button raised label="Login" onClick={handleLogin} />
       </div>
-      
     );
   }
 }
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isLoginView: true
+    }
+    this.changeView = this.changeView.bind(this);
+  }
+
+  changeView() {
+    this.setState({
+      isLoginView: !this.state.isLoginView
+    })
+  }
+  
   render() {
+    var visibleView;
+    if(this.state.isLoginView) {
+      visibleView = <LoginView/>
+    } else {
+      visibleView = <RegisterView/>
+    }
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to New Music Notifier</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {visibleView}
+        <Button flat label={this.state.isLoginView ? "Register" : "Login"} onClick={this.changeView} />
       </div>
     );
   }
